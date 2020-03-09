85f0362d388548c71d95c408d523517b816f1024
Bikram Sodhi <biksodhi@gmail.com>
2020-03-09 16:21:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,6 +18,10 @@ def assign_rooms(attendees)
 end
 
 def printer(print)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
 end
