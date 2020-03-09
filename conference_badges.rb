fe85ea48647a1471a18fac089af698e3e3bb1482
Bikram Sodhi <biksodhi@gmail.com>
2020-03-09 16:06:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  speaker.each_with_index.map do |attendee, index|
+  attendees.each_with_index.map do |attendee, index|
     "Hello, #{speaker}! You'll be assigned to room #{index+1}."
   end
 end
