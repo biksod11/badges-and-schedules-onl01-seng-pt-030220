d484ee90f6182663df6a9dd5c3d8177aca4344ee
Bikram Sodhi <biksodhi@gmail.com>
2020-03-09 15:50:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,12 @@ def batch_badge_creator(list)
   end
   arr
 end
+
+def assign_rooms(speaker)
+  rooms = []
+  counter = 1
+  speaker.each do |assignment| rooms << "Hello, #{speaker}! You'll be assigned to room #{counter}."
+    counter += 1
+  end
+  rooms
+end
